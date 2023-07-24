ae9dc401b705b9827c9362bd1f5f576ac1e967db
Bernd <bemue2@gmx.net>
2023-07-24 15:19:43 +0200

    fix for systemInfo

--- x/boxoffice/genesis.go
+++ x/boxoffice/genesis.go
@@ -9,9 +9,8 @@ import (
 // InitGenesis initializes the module's state from a provided genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// Set if defined
-	if genState.SystemInfo != nil {
-		k.SetSystemInfo(ctx, *genState.SystemInfo)
-	}
+	k.SetSystemInfo(ctx, genState.SystemInfo)
+
 	// Set all the storedConcert
 	for _, elem := range genState.StoredConcertList {
 		k.SetStoredConcert(ctx, elem)
@@ -28,7 +27,7 @@ func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	// Get all systemInfo
 	systemInfo, found := k.GetSystemInfo(ctx)
 	if found {
-		genesis.SystemInfo = &systemInfo
+		genesis.SystemInfo = systemInfo
 	}
 	genesis.StoredConcertList = k.GetAllStoredConcert(ctx)
 	// this line is used by starport scaffolding # genesis/module/export
